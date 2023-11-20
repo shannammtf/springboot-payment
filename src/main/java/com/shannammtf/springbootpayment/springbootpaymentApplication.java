3ed7e64fb01090e0d7e5aa9bcec96bb9bd9c35d3
Scaffolder <scaffolder@backstage.io>
2023-11-20 23:49:04 +0700

    initial commit

---
+++ src/main/java/com/shannammtf/springbootpayment/springbootpaymentApplication.java
@@ -0,0 +1,13 @@
+package com.shannammtf.springbootpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springbootpaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/shannammtf/springbootpayment/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shannammtf.springbootpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class springbootpaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
